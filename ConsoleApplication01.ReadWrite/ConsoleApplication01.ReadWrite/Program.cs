e3c25895e6945d861805e1ab5b1e40df41ac0c41
tompotola <jcvjetko@holisticware.com>
2015-03-05 21:41:54 +0100

    Tompotola's 1st Console Application

---
+++ ConsoleApplication01.ReadWrite/ConsoleApplication01.ReadWrite/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApplication01.ReadWrite
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			/*
+			string string_name = "Jana";
+			string string_greeting = "Bok";
+			*/
+
+			// Console.WriteLine (string_name + " " + string_greeting + "!");
+			// Console.WriteLine (string_greeting + " " + string_name + "!");
+
+			Console.Write ("What is your name?");
+			string line = Console.ReadLine ();
+
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.WriteLine ("Hi " + line);
+
+			Console.WriteLine ("Hi " + line.ToLower());
+			return;
+		}
+	}
+}
